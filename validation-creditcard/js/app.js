847509f5d8db922efcd207b3124d756ca0c08381
virmontor <virmontor@gmail.com>
2017-10-31 17:41:04 -0600

    Actualizando código

--- validation-creditcard/js/app.js
+++ validation-creditcard/js/app.js
@@ -1,31 +1,33 @@
 //Solicitando y validando dato de ingreso
-var NumberCard=prompt("Ingresa los dígitos de tu tarjeta");
+var numberCard=prompt("Ingresa los dígitos de tu tarjeta");
+
+var isValidCard = function(num){
 
 //Ingresar condicional para validar dato correcto
-if (typeof(NumberCard)==""){
+if (typeof(numberCard)===""){
   alert("Dato incorrrecto");
 }
 // Convirtiendo dato ingresado a un array
 var ArrayNum=[];
-  ArrayNum=Array.from(NumberCard);
+ArrayNum=Array.from(numberCard);
 
 //Invirtiendo array con reverse
-ArrayReverse=ArrayNum.reverse();
+  ArrayReverse=ArrayNum.reverse();
 
 //Ubicando números en índice impar y multiplicar por 2
 var arraymultiply=[];
+var ArrayResult=[];
   for (var i=0; i<ArrayReverse.length; i++){
     if (i%2!=0){
-    arraymultiply.push(ArrayReverse[i]*2);
-      } else {
-        arraymultiply.push(ArrayReverse[i]);
+      arraymultiply.push(ArrayReverse[i]*2);
+    }
+        if (arraymultiply[i]>=10){
+          ArrayResult.push(arraymultiply[i]-9);
+       } else {
+          arraymultiply.push(ArrayReverse[i]);
   }
 }
 //recorriendo con ciclo for arraymultiply
-for (var i=0; i<arraymultiply.length; i++){
-  if (arraymultiply[i] >=10){
+}
 
-  }
-
-
-console.log(arraymultiply);
+isValidCard(numberCard)
